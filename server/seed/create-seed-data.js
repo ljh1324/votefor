6d11a8a4b4a9f7677997edcc9ba561de9b03353c
hwanlee <ljhw3377@gmail.com>
2020-03-09 15:28:35 +0900

    feat: seed 데이터 등록 코드 및 정적 파일 서비스
    - seed 데이터 등록 코드 작성
    - `npm run seed:dev`를 통하여 개발용 DB에 seed 데이터 등록
    - 이미지 파일 제공을 위한 정적 파일 서비스
    - 라우터에서 아무런 요청이 발생하지 않은 URL인 경우일 빈 객체 응답

---
+++ server/seed/create-seed-data.js
@@ -0,0 +1,79 @@
+import fs from 'fs';
+import path from 'path';
+
+import '../src/config/env';
+import db from '../src/database';
+
+const contents = fs.readFileSync(path.join(__dirname, 'seed-data.json'));
+const { categories, parties, promises } = JSON.parse(contents);
+/**
+ * categories: [
+ *  {
+ *    name: ""        카테고리명
+ *  }
+ * ]
+ *
+ * parties: [
+ *  {
+ *    name: "",       정당명
+ *    image: ""       정당 로고 파일 위치
+ *  }
+ * ]
+ *
+ * promises: [
+ *  {
+ *    category: "",   카테고리명
+ *    party: "",      정당명
+ *    summary: "",    정책 요약
+ *    contents: ""    정책 내용
+ *  }
+ * ]
+ */
+
+const createCategoriesData = async () => {
+  const results = await db.Category.bulkCreate(categories);
+
+  return results;
+};
+
+const createPartiesData = async () => {
+  const results = await db.PoliticalParty.bulkCreate(parties);
+
+  return results;
+};
+
+const createPromisesData = async () => {
+  const categoryList = await db.Category.findAll({});
+  const categoryObj = categoryList.reduce((acc, item) => {
+    const { name, no } = item;
+    acc[name] = no;
+    return acc;
+  }, {});
+
+  const partyList = await db.PoliticalParty.findAll({});
+  const partyObj = partyList.reduce((acc, item) => {
+    const { name, no } = item;
+    acc[name] = no;
+    return acc;
+  }, {});
+
+  promises.forEach((item, idx) => {
+    const { category, party } = item;
+    promises[idx].category_no = categoryObj[category];
+    promises[idx].political_party_no = partyObj[party];
+  });
+
+  const results = await db.ElectionPromise.bulkCreate(promises);
+
+  return results;
+};
+
+const createSeedData = async () => {
+  await db.sequelize.sync();
+
+  await createCategoriesData();
+  await createPartiesData();
+  await createPromisesData();
+};
+
+createSeedData();
--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import status from 'http-status';
 
 import v1 from './v1';
-import db from './database';
 import logger from './lib/logger';
 
 morgan.format(
   'combined',
-  ':method :url :status :res[content-length] - :response-time ms :remote-addr - :remote-user :referrer :user-agent',
+  ':method :url :status :res[content-length] - :response-time ms :remote-addr - :remote-user :referrer :user-agent'
 );
 
 const app = express();
@@ -19,11 +19,16 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static('public'));
 app.use('/', v1);
+
+app.use((req, res, next) => {
+  return res.status(status.NOT_FOUND).json({});
+});
 
 app.use((err, req, res, next) => {
   logger.error(err);
-  return res.status(500).json(INTERNAL_SERVER_ERROR_EXCEPTION);
+  return res.status(status.INTERNAL_SERVER_ERROR).json({});
 });
 
 export default app;

f3b3b8f5433b364ef6ee0e5527f70ad11b7f4820
hwanlee <ljhw3377@gmail.com>
2020-03-15 23:56:36 +0900

    feat: 원 그래프 컴포넌트 추가
    - 투표 결과의 정당별 비율을 원 그래프로 보여주기 위함

---
+++ frontend/src/components/CircleGraph/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+import * as S from "./styled";
+
+const polarToCartesian = (centerX, centerY, radius, degrees) => {
+  const radians = ((degrees - 90) * Math.PI) / 180.0;
+  return {
+    x: centerX + radius * Math.cos(radians),
+    y: centerY + radius * Math.sin(radians)
+  };
+};
+
+const calculateAngleRanges = (nums, total) => {
+  let startAngle = 0;
+  let angleRanges = [];
+
+  nums.forEach(num => {
+    let angle = (num / total) * 360;
+    angleRanges.push({
+      start: startAngle,
+      end: startAngle + angle
+    });
+    startAngle += angle;
+  });
+
+  return angleRanges;
+};
+
+const CircleGraph = ({ nums, items, unit, total, width, padding }) => {
+  const cx = width / 2 + padding;
+  const cy = width / 2 + padding;
+  const radius = width / 2 - padding * 2;
+
+  const angleRanges = calculateAngleRanges(nums, total);
+
+  const renderingCircleGraph = angleRanges.map(({ start, end }, idx) => {
+    const startPoint = polarToCartesian(cx, cy, radius, end);
+    const endPoint = polarToCartesian(cx, cy, radius, start);
+    const largeArcFlag = end - start <= 180 ? "0" : "1";
+
+    const d = [
+      "M",
+      startPoint.x,
+      startPoint.y,
+      "A",
+      radius,
+      radius,
+      0,
+      largeArcFlag,
+      0,
+      endPoint.x,
+      endPoint.y,
+      "L",
+      cx,
+      cy,
+      "Z"
+    ].join(" ");
+
+    return <path d={d} stroke="white" strokeWidth="1" fill={items[idx].color}></path>;
+  });
+
+  const renderingItems = items.map(({ name, color }) => (
+    <S.ItemWrapper>
+      <S.Color color={color} />
+      <S.Name>{name}</S.Name>
+    </S.ItemWrapper>
+  ));
+
+  return (
+    <S.GraphWrapper>
+      <svg width={width} height={width}>
+        {renderingCircleGraph}
+      </svg>
+      <S.DescWrapper>{renderingItems}</S.DescWrapper>
+    </S.GraphWrapper>
+  );
+};
+
+export default CircleGraph;
---
+++ frontend/src/components/CircleGraph/styled.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const GraphWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 600px;
+  border-radius: 5px;
+  box-sizing: border-box;
+  margin: 10px 0;
+  justify-content: center;
+`;
+
+const DescWrapper = styled.div`
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Name = styled.span`
+  font-size: 1rem;
+`;
+
+const Color = styled.div`
+  width: 20px;
+  height: 20px;
+  background: ${props => props.color};
+  border-radius: 20px;
+  margin: 0 10px;
+`;
+
+export { GraphWrapper, DescWrapper, ItemWrapper, Name, Color };

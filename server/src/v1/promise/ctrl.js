aab6f790af807bd01ee1694584c358f3bdd6788f
hwanlee <ljhw3377@gmail.com>
2020-03-18 00:20:17 +0900

    feat: 공약을 분야별로 분류하여 응답
    - 프론트엔드에서 데이터를 받아 바로 사용할 수 있도록 함

--- server/src/v1/promise/ctrl.js
+++ server/src/v1/promise/ctrl.js
@@ -3,12 +3,41 @@ import db from '../../database';
 const read = async (req, res, next) => {
   try {
     const promises = await db.ElectionPromise.findAllPromises();
-    promises.forEach((_, idx) => {
-      delete promises[idx].category_no;
-      delete promises[idx].political_party_no;
+    const categories = {};
+    const parties = {};
+
+    promises.forEach(promise => {
+      const { summary, contents } = promise;
+      const categoryName = promise['Category.name'];
+      const partyName = promise['PoliticalParty.name'];
+      let color;
+      let image;
+
+      if (!(partyName in parties)) {
+        image = promise['PoliticalParty.image'];
+        color = promise['PoliticalParty.color'];
+        parties[partyName] = {
+          name: partyName,
+          color,
+          image
+        };
+      }
+      if (!(categoryName in categories)) {
+        categories[categoryName] = {
+          voted: false,
+          promises: []
+        };
+      }
+
+      categories[categoryName].promises.push({
+        summary,
+        contents,
+        voted: false,
+        party: parties[partyName]
+      });
     });
 
-    res.json({ promises });
+    res.json({ categories });
   } catch (err) {
     next(err);
   }

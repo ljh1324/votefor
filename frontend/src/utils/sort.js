62f2f03149dcd391c74c4cc5dbe0fd0c5f8d6ca7
hwanlee <ljhw3377@gmail.com>
2020-04-03 23:07:33 +0900

    chore: console.log 삭제
    - 필요없는 코드 삭제

--- frontend/src/utils/sort.js
+++ frontend/src/utils/sort.js
@@ -1,9 +1,7 @@
 export const sortBy = (list, key) => {
   list.sort((a, b) => {
-    console.log(a[key]);
     if (a[key] < b[key]) return -1;
     else if (a[key] === b[key]) return 0;
     else return 1;
   });
-  console.log(list);
 };

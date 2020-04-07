e1850685217e8d5666e39aa5efc36513064294a4
hwanlee <ljhw3377@gmail.com>
2020-04-07 20:45:20 +0900

    feat: ElectionPromise 스타일 변경
    - max-height를 auto로 설정

--- frontend/src/components/ElectionPromise/styled.js
+++ frontend/src/components/ElectionPromise/styled.js
@@ -16,7 +16,7 @@ const ContentsWrapper = styled.div`
   background: #fff;
 
   transition: all 0.9s ease;
-  max-height: ${props => (props.isClicked ? "10000px" : "0px")};
+  max-height: ${props => (props.isClicked ? "auto" : "0")};
   opacity: ${props => (props.isClicked ? "1" : "0")};
 `;
 
